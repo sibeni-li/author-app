8d9d322b28d2ebfbc54c692c69bde0f5b6838040
sibeni-li <pro@sibeni.li>
2025-01-13 14:44:35 +0100

    Import stylesheets

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import About from './pages/About';
 import Error from './pages/Error';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import './App.css'
+import './styles/App.scss'
 
 function App() {
   return (
