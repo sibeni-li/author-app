0c9adc7b3b36b18ed0122a68b16b28898d3961c9
sibeni-li <pro@sibeni.li>
2025-01-13 16:01:13 +0100

    Add Banner

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,18 @@
+import '../../styles/Home.scss'
+
 function Home() {
-    return <h1>Home</h1>
+    return (
+        <div>
+            <div className="banner div">
+                <div className="title">
+                    <h1>Bienvenue dans mon univers</h1>
+                    <h2>L&apos;univers d&apos;un cerveau...</h2>
+                </div>
+            </div>
+            <div className='div'>
+                <h2>Damnatus</h2>
+            </div>
+        </div>
+    )
 };
 export default Home;
