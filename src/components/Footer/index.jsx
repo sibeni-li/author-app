6cc0dc07577fb05f9143f72912ab82bda89bc49c
sibeni-li <pro@sibeni.li>
2025-01-13 15:59:57 +0100

    Fix syntax

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -4,11 +4,9 @@ import '../../styles/Footer.scss'
 function Footer() {
     return (
     <footer>
-        <div>
-            <img src={LOGO} className='logo-footer' alt='Logo M.S-Ramety'/>
-            <p>© 2024 M.S-Ramety</p>
-            <p>Tous droits réservés</p>
-        </div>
+        <img src={LOGO} className='logo-footer' alt='Logo M.S-Ramety'/>
+        <p>© 2024 M.S-Ramety</p>
+        <p>Tous droits réservés</p>
     </footer>
     )
 };
