cc49d9f6f3a84baa0609063ddd29f41cb68119d9
sibeni-li <pro@sibeni.li>
2025-01-15 21:49:46 +0100

    customize md to html converter added

---
+++ src/markdownConverter.js
@@ -0,0 +1,91 @@
+function getTypeLine(line) {
+    const levelHeader = (line.match(/#/g) || []).length;
+    if (line.startsWith('#') && line[levelHeader] === ' ' && levelHeader <= 6) {
+        return 'Header';
+    } else if (line.startsWith('[!lore]: ')) {
+        return 'Lore';
+    } else if (line.startsWith('[!past]: ')) {
+        return 'Past';
+    } else if (line.startsWith('[!dial]: ')) {
+        return 'Dial';
+    } else {
+        return 'Paragraph';
+    }
+}
+
+function headerConvert(mdText) {
+    const levelHeader = (mdText.match(/#/g) || []).length;
+    const header = mdText.replace(/#/g, '');
+    const lh = Math.min(levelHeader, 6);
+    return `<h${lh}>${header}</h${lh}>`;
+}
+
+function loreConvert(mdText) {
+    const lore = mdText.replace('[!lore]: ', '');
+    return `<p class="lore">${lore}</p>`;
+}
+
+function pastConvert(mdText) {
+    let past = mdText.replace('[!past]: ', '');
+    past = past.replace(/\n/g, '</br>');
+    return `<p class="past">${past}</p>`;
+}
+
+function dialConvert(mdText) {
+    let dial = mdText.replace('[!dial]: ', '');
+    dial = dial.replace(/\n/g, '</br></br>');
+    return `<p class="dial">${dial}</p>`;
+}
+
+function boldConvert(mdText) {
+    let boldHtml = '';
+    if ((mdText.match(/\*\*/g) || []).length % 2 === 0) {
+        const x = mdText.split('**');
+        x.forEach((o, i) => {
+            if (i % 2 === 0) boldHtml += o;
+            else boldHtml += '<strong>' + o + '</strong>';
+        });
+    } else {
+        boldHtml = mdText;
+    }
+    return boldHtml;
+}
+
+function italicConvert(mdText) {
+    let italicHtml = '';
+    if ((mdText.match(/\*/g) || []).length % 2 === 0) {
+        const x = mdText.split('*');
+        x.forEach((o, i) => {
+            if (i % 2 === 0) italicHtml += o;
+            else italicHtml += '<i>' + o + '</i>';
+        });
+    } else {
+        italicHtml = mdText;
+    }
+    return italicHtml;
+}
+
+function textConvert(inp) {
+    const lines = inp.split('\n\n');
+    let html = lines.map(o => {
+        switch(getTypeLine(o)) {
+            case 'Header':
+                return headerConvert(o);
+            case 'Lore':
+                return loreConvert(o);
+            case 'Past':
+                return pastConvert(o);
+            case 'Dial':
+                return dialConvert(o);
+            default:
+                return `<p>${o}</p>`;
+        }
+    });
+
+    html = html.map(line => boldConvert(line));
+    html = html.map(line => italicConvert(line));
+    
+    return html.join('\n');
+}
+
+export {textConvert};
