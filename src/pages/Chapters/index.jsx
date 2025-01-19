056937f4829b8b4b97517715b6d22c3693229123
sibeni-li <pro@sibeni.li>
2025-01-19 23:54:58 +0100

    Chapters page added to fetch the good chapter and return Chapter component

--- src/pages/Chapters/index.jsx
+++ src/pages/Chapters/index.jsx
@@ -1,5 +1,46 @@
-function Chapter() {
-    return <h1>Chapter</h1>
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from 'react';
+import Error from '../Error/index.jsx';
+import Chapter from "../../components/Chapter";
+import '../../styles/Chapters.scss'
+
+function Chapters() {
+    const { id } = useParams();
+    const [chapter, setChapter] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        fetch('/src/stories.json')
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then(data => {
+                const foundChapter = data.chapters.find(chapter => chapter.id === id);
+                if (foundChapter) {
+                    setChapter(foundChapter);
+                } else {
+                    setError('Chapter not found');
+                }
+                setLoading(false);
+            })
+            .catch(error => {
+                setError(error.message);
+                setLoading(false);
+            });
+    }, [id]);
+
+    if (loading) return <div>Loading...</div>;
+    if (error) return <div>Error: {error}</div>;
+
+    return (
+        <div className="read-chapter">
+            {chapter ? <Chapter chapter={chapter} /> : <Error />}
+        </div>
+    );
 };
 
-export default Chapter;
+export default Chapters;
