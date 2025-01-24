9a5daad511b0f74f49a4127836778bb73fa20904
sibeni-li <pro@sibeni.li>
2025-01-24 20:34:29 +0100

    Error component added

--- src/pages/Error/index.jsx
+++ src/pages/Error/index.jsx
@@ -1,5 +1,13 @@
+import { Link } from "react-router-dom";
+import '../../styles/Error.scss'
+
 function Error() {
-    return <h1>Error</h1>
+    return (
+        <div className="error">
+            <h1>Oups...<br/>La page que vous cherchez n&apos;existe pas!</h1>
+            <Link to='/'>Retourner à la page d&apos;accueil</Link>
+        </div>
+    )
 };
 
 export default Error;
