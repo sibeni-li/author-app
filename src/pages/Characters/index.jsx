7461fa9619ee6a547c15fe974622d279fb69f716
sibeni-li <pro@sibeni.li>
2025-01-24 22:05:02 +0100

    Characters added

--- src/pages/Characters/index.jsx
+++ src/pages/Characters/index.jsx
@@ -1,4 +1,39 @@
-function Universeandcharacters() {
-    return <h1>characters</h1>
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import '../../styles/Characters.scss'
+
+
+function Characters() {
+    const [characters, setCharacters] = useState([]);
+        const [loading, setLoading] = useState(true);
+        const [error, setError] = useState(null);
+    
+        useEffect(() => {
+            fetch('/src/assets/data/characters.json')
+                .then(response => {
+                    if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                    }
+                    return response.json();
+                })
+                .then(data => {
+                    setCharacters(data);
+                    setLoading(false);
+                })
+                .catch(error => {
+                    setError(error.message);
+                    setLoading(false);
+                });
+        }, []);
+
+        if (loading) return <div>Loading...</div>;
+        if (error) return <div>Error: {error}</div>;
+        console.log(characters)
+    return (
+        <div className="characters">
+            <h1 className="characters-title">Les Personnages</h1>
+            <ul className="characters-list">{characters.map((character) => <li className="characters-list__element" key={character.id}><Link to='/' >{character.name}</Link></li>)}</ul>
+        </div>
+    )
 };
-export default Universeandcharacters;
+export default Characters;
